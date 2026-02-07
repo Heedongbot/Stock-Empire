c3f3f87623e97cdf518a2706f4da893c3c169cb9
Heedongbot <66683300hd@gmail.com>
2026-02-07 21:51:34 +0900

    Emergency: Remove middleware.ts entirely to bypass 500 error

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-// 미들웨어 로직을 완전히 비워서 500 에러 원인을 차단함
-export default clerkMiddleware();
-
-export const config = {
-    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
