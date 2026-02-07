e205ebd9b72959bac9426e4ad46b067c6d79dd40
Heedongbot <66683300hd@gmail.com>
2026-02-07 21:18:35 +0900

    Fix: Explicitly pass Clerk publishableKey to fix Vercel prerender error

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider localization={koKR}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY} localization={koKR}>
       <html lang="ko" suppressHydrationWarning>
         <head>
           <script
